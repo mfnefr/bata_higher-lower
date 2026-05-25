c664d5eaf0124bb80b0546dbc4eb5a1f037becc5
mfnefr <164998182+mfnefr@users.noreply.github.com>
2026-05-25 11:50:24 +0200

    add: little changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdam.production02.nmh.nmheagle.sk%2Fapi%2Fimage%2F1280x720%2F6c8%2F6c8c32a4-b882-461e-a409-0e7427dbbfb2.jpeg&f=1&nofb=1&ipt=b762726e7e78395f3c00da1b1dad70b997eaca1a4f34a94f22055370d29f0664')]
-             bg-cover bg-center bg-fixed bg-black/70 bg-blend-overlay antialiased text-gray-900">
+             bg-cover bg-center bg-fixed bg-black/50 bg-blend-overlay antialiased text-gray-900">
 
     {{ $slot }}
 
