bae9e258bdb5ba5680604d080953bc33c7a89789
mfnefr <164998182+mfnefr@users.noreply.github.com>
2026-05-25 11:43:13 +0200

    add: little changes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Game;
 
-Route::get('/game', Game::class);
+Route::get('/', Game::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
